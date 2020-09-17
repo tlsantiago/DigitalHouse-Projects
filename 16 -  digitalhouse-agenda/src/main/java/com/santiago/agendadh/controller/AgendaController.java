e7c10d9cf270b2805b46ef6a30bfba6d4ab43f17
Santiago <ssantiagotl@gmail.com>
2020-09-17 13:59:18 -0300

    Updating folder name + front end development

--- 16 -  digitalhouse-agenda/src/main/java/com/santiago/agendadh/controller/AgendaController.java
+++ 16 -  digitalhouse-agenda/src/main/java/com/santiago/agendadh/controller/AgendaController.java
@@ -3,6 +3,7 @@ package com.santiago.agendadh.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.santiago.agendadh.model.Agenda;
 import com.santiago.agendadh.repository.AgendaRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/agenda")
 public class AgendaController {
